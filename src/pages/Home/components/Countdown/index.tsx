cc4e23b6463a26bf97e92e642708ad0c930d4e3f
ikaueMatos <kaue178hotimail@gmail.com>
2023-08-10 07:46:26 -0300

    Removendo useState

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { CountdownContainer, Separator } from "./styled";
 import { differenceInSeconds } from "date-fns";
 import { CyclesContext } from "../..";
